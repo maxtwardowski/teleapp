d30f7e646ca1b23971c98950079a2e87dd3cc88e
maxtwardowski <twardowskimax@gmail.com>
2018-04-22 23:08:29 +0200

    Some basic classes created

---
+++ TelephoneNumber.java
@@ -0,0 +1,13 @@
+public class TelephoneNumber implements Comparable<TelephoneNumber> {
+
+    int countryCode, localNumber;
+
+    public int compareTo(TelephoneNumber otherNumber) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
